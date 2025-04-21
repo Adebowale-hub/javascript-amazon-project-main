8facc1b3740b4a3bcf6d9abfea052e7fe670ebdb
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-04-21 16:26:56 +0100

    Save cart in localStorage even we change and go to a different page

--- data/cart.js
+++ data/cart.js
@@ -6,16 +6,31 @@
 // Export the cart
 // To make the checkout page interactive
 // Checkout Page Step 1: Create inside the cart some default values
-export let cart = [
-    {
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1,
-    },
-    {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
-    },
-];
+
+// Get the cart from the local Storage instead of using default values.
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    // Use Default values for products in the cart array to avoid null : export let cart = [];
+    cart = [
+        {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 1,
+        },
+        {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1
+        }
+    ];
+}
+
+
+
+// Save our cart to the local storage and don4t need to reset or to refresh the page.
+function saveToStorage() {
+    // What we want to save = 'cart' and convert it into a string 
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // Step 9: Create a Function to add products in the cart
 // Step 10: Put related code together 
@@ -40,6 +55,8 @@ export function addToCart(productId) {
         });
         // console.log(cart.length);
     }
+    // After adding products to the cart, save them to the local storage.
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -53,4 +70,7 @@ export function removeFromCart(productId) {
    });
 
    cart = newCart;
+
+   // Remove function finished and save to the local storage
+   saveToStorage();
 }
