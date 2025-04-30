e29cf21a6d4e4e2406bb81cc50eb3f9f0d6f416a
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-04-30 10:15:05 +0100

    Bugs fixed for saving cart in localStorage

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -16,6 +16,8 @@ import { cart, addToCart } from '../data/cart.js';
 import { products } from '../data/products.js'; 
 import { formatCurrency } from './utils/money.js';
 
+// hello();
+
 // Step 1: Save the data
 // This is called a data structure.
 /* const products = [
