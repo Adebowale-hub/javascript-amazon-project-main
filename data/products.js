1708ccda7b45af2dcebb14a9b9d3f52b5e0b9e7d
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-05-31 10:48:53 +0100

    Convert products into Classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
 // getProduct Function that match the matchingProduct to the product
 export function getProduct(productId) {
   let matchingProduct;
@@ -11,7 +13,49 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
+// Converting an object into a class
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
 
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+/* const product1 = new Product({
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  });
+console.log(product1); */
+
+// .map((productDetails) => {}) Loops in the array and transfoms each value of the array into a product class
 export const products = [
   // Also export products.js as a module
   {
@@ -672,4 +716,7 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
+// console.log(products);
