364a43a8de04b97946b068dd0e1cf139b0df0824
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-02-19 02:22:30 +0100

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -31,7 +31,8 @@ cart.forEach((cartItem) => {
 
     // Store the generated HTML into the variable to appear (use of the accumulation pattern '+=')
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -122,5 +123,11 @@ document.querySelectorAll('.js-delete-link')
             const productId = link.dataset.productId;
             removeFromCart(productId);
             // console.log(cart);
+
+            const container = document.querySelector(
+                `.js-cart-item-container-${productId}`
+            );
+            // console.log(container);
+            container.remove();
         });
     });
